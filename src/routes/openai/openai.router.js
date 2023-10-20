92906bc98dcb1609e231b1fcb4b082f775b71d2d
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2023-10-20 00:19:35 -0400

    added rate limiter

--- src/routes/openai/openai.router.js
+++ src/routes/openai/openai.router.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const { rateLimit } = require('express-rate-limit');
+
+
 const openAIRouter = express.Router();
 const { httpCreateImage } = require("./openai.controller");
 const wrapAsync = require("../../errorHandling/wrapAsync");
 
-openAIRouter.get("/createimage", wrapAsync(httpCreateImage));
+
+const openAILimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    max: 10, // start blocking after 10 requests
+    message:
+        "Too many requests created from this IP, please try again after an hour"
+});
+
+
+
+openAIRouter.get("/createimage", openAILimiter, wrapAsync(httpCreateImage));
 
 module.exports = openAIRouter;
